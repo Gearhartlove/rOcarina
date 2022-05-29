7469d35ee9d5e51d3fc4ed9c0118e1bf4535b03a
Kristoff <finley@kristoff.codes>
2022-05-29 11:45:45 +0200

    Basic foundations created.

--- ocarina_plugin/src/lib.rs
+++ ocarina_plugin/src/lib.rs
@@ -1,8 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::ops::Deref;
+use bevy::prelude::*;
+
+// q: what happens when there is no controls?
+// -> None Enum varient?
+
+const DEFAULT_CONTROLS: CustomControls = default_controls();
+
+pub fn ocarina(keys: Res<Input<KeyCode>>, mut custom_controls: Option<ResMut<CustomControls>>, audio: Res<Audio>,
+               asset_server: Res<AssetServer>) {
+    // check if custom controls are defined or not
+    let controls: CustomControls = setup_controls(custom_controls);
+
+    // handle user inputs
+    if keys.pressed(controls.d_note) {
+        let note: Handle<AudioSource> =
+            asset_server.load("ocarina_plugin_assets/ocarina_d_note.ogg");
+        audio.play(note);
+    }
+}
+
+fn setup_controls(custom_controls: Option<ResMut<CustomControls>>) -> CustomControls {
+    if let None = custom_controls {
+        return DEFAULT_CONTROLS;
+    }
+    return *custom_controls.unwrap().deref();
+}
+
+#[derive(Clone, Copy)]
+pub struct CustomControls {
+    d_note: KeyCode,
+    a_sharp_note: KeyCode,
+}
+
+const fn default_controls() -> CustomControls {
+    CustomControls {
+        d_note: KeyCode::D,
+        a_sharp_note: KeyCode::A,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use bevy::prelude::*;
 
+// source : http://soundfxcenter.com/download-sound/ocarina-of-time-notes-flute-d-long-sound-effect/
+
 fn main() {
     let mut app: App = App::new();
     app
         .add_plugins(DefaultPlugins)
+        // .add_resource(ocarina_plugin::CustomControls {}) // optional
+        .add_system(ocarina_plugin::ocarina)
         // .add_plugin(Ocarina)
         .run();
 }
+
+// todo: hold user input
+// fn user_input(keys: Res<Input<KeyCode>>, audio: Res<Audio>, asset_server: Res<AssetServer>) {
+//     if keys.pressed(KeyCode::A) {
+//         let note: Handle<AudioSource> = asset_server.load("ocarina_plugin/assets/ocarina_d_note.ogg");
+//         audio.play(note);
+//     }
+// }
